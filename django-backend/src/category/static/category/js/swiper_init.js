b1c855a3f2eea156e9159768c7e19fcd61977eb9
romberto <korhoff@mail.ru>
2023-10-21 12:45:41 +0400

    версия с помидорами

--- django-backend/src/category/static/category/js/swiper_init.js
+++ django-backend/src/category/static/category/js/swiper_init.js
@@ -12,9 +12,9 @@ window.addEventListener('load', function(){
         shadowOffset: 20,
         shadowScale: 0.94,
       },
-      autoplay: {
-        delay: 3000,
-      },
+//      autoplay: {
+//        delay: 3000,
+//      },
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
--- django-backend/src/proj/settings.py
+++ django-backend/src/proj/settings.py
@@ -28,12 +28,14 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '188.225.72.113', '1566825-cw73062.twc1.net', '127.0.0.1']
-
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
+if DEBUG:
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS = ['localhost', '188.225.72.113', '1566825-cw73062.twc1.net', '127.0.0.1']
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
 
 # Application definition
 
