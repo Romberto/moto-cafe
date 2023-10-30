48b140f1c2598fb7d45521704eab6e9d3a69f5bc
romberto <korhoff@mail.ru>
2023-10-30 18:01:02 +0400

    v2.1 работа с db

--- django-backend/src/proj/settings.py
+++ django-backend/src/proj/settings.py
@@ -93,13 +93,20 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.environ.get('DATABASE_URL', os.path.join(BASE_DIR, 'db.sqlite3')),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
