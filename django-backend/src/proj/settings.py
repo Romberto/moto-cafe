5e0c9e0321a5eb54c2048aaa57e31842d50e8f62
romberto <korhoff@mail.ru>
2023-10-31 10:05:23 +0400

    v2 работа с db

--- django-backend/src/proj/settings.py
+++ django-backend/src/proj/settings.py
@@ -93,18 +93,19 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if DEBUG:
+
+if not DEBUG:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.environ.get('DATABASE_URL', os.path.join(BASE_DIR, 'db.sqlite3')),
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
         }
-    }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': BASE_DIR / './persistentdata/db/db.sqlite3',
         }
     }
 
