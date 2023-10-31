29dc626bc963b0dd5b4a49a355cb4f7c80742822
romberto <korhoff@mail.ru>
2023-10-31 15:19:26 +0400

    переход на postgresql

--- django-backend/src/proj/settings.py
+++ django-backend/src/proj/settings.py
@@ -96,11 +96,15 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'moto_base',
+        'USER': 'moto_db_user',
+        'PASSWORD': 'wer324cvG',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+}
 
 
 # Password validation
