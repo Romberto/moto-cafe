56bd1c4d05eba0d5c63525ade847ae0a0e9d1cf0
root <root@1566825-cw73062.twc1.net>
2023-12-08 16:32:39 +0300

    fyfel

--- django-backend/src/proj/asgi.py
+++ django-backend/src/proj/asgi.py
@@ -8,16 +8,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
-import sys
-print(sys.path)
-application = get_asgi_application()
+django.setup()
+application = get_default_application()
 import admin_p.routing
 
 application = ProtocolTypeRouter({
