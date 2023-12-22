a1e81df3bb86ddbcfd30cf607cb364a9a3146b66
root <root@1566825-cw73062.twc1.net>
2023-12-22 11:43:27 +0300

    www

--- django-backend/src/proj/settings.py
+++ django-backend/src/proj/settings.py
@@ -15,7 +15,7 @@ load_dotenv()
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1', '188.225.72.113', '1566825-cw73062.twc1.net']
