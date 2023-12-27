358bf2665bbb57ec524aba92f664e72bc1161732
romberto <korhoff@mail.ru>
2023-12-27 17:38:29 +0400

    работа с swiper

--- django-backend/src/category/static/category/js/menu_swiper_init.js
+++ django-backend/src/category/static/category/js/menu_swiper_init.js
@@ -45,41 +45,19 @@ $('.accordion-header').on('click', function(e){
                     category_block.append(item_block)
                 })
 
-                var elements = document.querySelectorAll('.swiper[class*="mySwiper_"]');
 
-                // Создаем массив для хранения классов
-                var mySwiperClasses = [];
-
-                // Проходим по найденным элементам
-                elements.forEach(function(element) {
-                    // Получаем все классы элемента
-                    var classNames = element.className.split(' ');
-
-                    // Фильтруем классы, начинающиеся с "mySwiper_"
-                    var mySwiperClassesForElement = classNames.filter(function(className) {
-                        return className.indexOf('mySwiper_') === 0;
+                var swiper = new Swiper('.mySwiper', {
+                    direction: "vertical",
+                    speed: 1200,
+                    loop: true,
+                    spaceBetween: 10,
+                    pagination: {
+                        el: `.swiper-pagination`,
+                        clickable: true,
+                    },
+                //    autoHeight: true, // Включение автоматической высоты
                     });
 
-                    // Добавляем найденные классы в общий массив
-                    mySwiperClasses = mySwiperClasses.concat(mySwiperClassesForElement);
-                });
-
-                // В результате mySwiperClasses содержит все классы, начинающиеся с "mySwiper_"
-                mySwiperClasses.forEach(function(element){
-                    var resultArray = element.split('_')[1];
-
-                    var swiper = new Swiper(`.${element}`, {
-                        direction: "vertical",
-                        speed: 1200,
-                        loop: true,
-                        spaceBetween: 10,
-                        pagination: {
-                            el: `.swiper-pagination${resultArray}`,
-                            clickable: true,
-                        },
-                    //    autoHeight: true, // Включение автоматической высоты
-                        });
-                });
 
 
 
